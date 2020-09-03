5cfa23c0eb0b1b8279f28d11c3021e79db6d6342
Viacheslav Kupriyanov <vitya39@gmail.com>
2020-09-03 21:26:10 +0300

    HTML page parsing

---
+++ src/main/java/quartz/SqlRuParse.java
@@ -0,0 +1,30 @@
+package quartz;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * Class SqlRuPars.
+ * @author v.ivanov
+ * @version 1
+ * @since 03.09.2020
+ */
+public class SqlRuParse {
+    /**
+     * Entry point.
+     * @param args args
+     * @throws Exception thrown exception
+     */
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("http://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td: row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(td.lastElementSibling().text());
+        }
+    }
+}
